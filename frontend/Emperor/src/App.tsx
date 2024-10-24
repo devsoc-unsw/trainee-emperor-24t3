803f0ce8a9298cbec9201f093ad8f8895bb03eed
Winnie Zhang <winnie.zhang825@gmail.com>
2024-10-24 13:27:35 +1100

    set up frontend and landing

---
+++ frontend/Emperor/src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import { Landing } from './pages/Landing'
+
+function App() {
+
+  return (
+    <>
+    <Landing/>
+{/* 
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<p>hello</p>} />
+      </Routes>
+    </BrowserRouter> */}
+    </>
+  )
+}
+
+export default App
---
+++ frontend/Emperor/src/pages/Landing.tsx
@@ -0,0 +1,27 @@
+import Image from "../assets/5047210.png"
+
+
+export const Landing = () => {
+
+    return (
+        <>
+        <div style={
+            {
+                display: 'flex',
+                flexDirection: 'row',
+            }
+        }>
+
+        
+        <div >
+        <p>ORGANISE LIFE WITH SYNCIETY</p>
+        <p>Live in a society where you can stay synced with
+        each other.</p>
+        </div>
+        <div>
+            <img src={Image} alt="connection image" />
+        </div>
+        </div>
+        </>
+    )
+}
