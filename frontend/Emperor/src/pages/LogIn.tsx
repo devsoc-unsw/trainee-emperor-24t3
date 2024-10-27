276c8a1ca5361fc78ee9881de0169f9c4d4b2bb1
Eric Lu <eric.lukw@gmail.com>
2024-10-27 23:29:11 +1100

    Centered login div

--- frontend/Emperor/src/pages/LogIn.tsx
+++ frontend/Emperor/src/pages/LogIn.tsx
@@ -6,40 +6,53 @@ export const LogIn = () => {
     return (
         <>
             <Box sx = {{
-                position: 'absolute',
-                top: '0',
-                left: '10px',
-            }}>
-                <p>Synciety</p>
-            </Box>
-
-            <Box sx = {{
-                display: 'flex',
-                borderRadius: '10px',
-                overflow: 'hidden',
-                width: '70vw',
-                height: '500px',
+                minHeight: '100vh',
+                backgroundColor: 'white',
             }}>
                 <Box sx = {{
-                    p: 2,
-                    backgroundColor: '#EAEBEB',
-                    color: 'black',
-                    width: '50%',
+                    position: 'absolute',
+                    top: '0',
+                    left: '10px',
                 }}>
-                    <p>Login</p>
-                    <p>Username</p>
-                    <p>Password</p>
+                    <p>Synciety</p>
                 </Box>
+
                 <Box sx = {{
-                    p: 2,
-                    backgroundColor: '#AA7CD2',
-                    color: 'black',
-                    borderLeft: 1,
+                    position: 'absolute',
+                    display: 'flex',
+                    borderRadius: '10px',
+                    border: 2,
+                    overflow: 'hidden',
                     width: '50%',
+                    height: '500px',
+                    margin: '0',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
                 }}>
-                    <p>Hello</p>
-                    <p>New User?</p>
-                    <p>Sign Up</p>
+                    <Box sx = {{
+                        p: 2,
+                        backgroundColor: '#EAEBEB',
+                        color: 'black',
+                        width: '50%',
+                        height: '500px',
+                    }}>
+                        <p>Login</p>
+                        <p>Username</p>
+                        <p>Password</p>
+                    </Box>
+                    <Box sx = {{
+                        p: 2,
+                        backgroundColor: '#AA7CD2',
+                        color: 'black',
+                        borderLeft: 2,
+                        width: '50%',
+                        height: '500px',
+                    }}>
+                        <p>Hello</p>
+                        <p>New User?</p>
+                        <p>Sign Up</p>
+                    </Box>
                 </Box>
             </Box>
         </>
