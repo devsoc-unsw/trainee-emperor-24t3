a81a08e57394941ba716158e20350526e2053adf
Eric Lu <eric.lukw@gmail.com>
2024-10-30 12:34:01 +1100

    Finished Login draft

--- frontend/Emperor/src/pages/LogIn.tsx
+++ frontend/Emperor/src/pages/LogIn.tsx
@@ -1,4 +1,5 @@
-import { Box, Typography, Button, createTheme, ThemeProvider, TextField } from "@mui/material"
+import { Box, Typography, Button, createTheme, ThemeProvider, Checkbox } from "@mui/material"
+import '../App.css'
 
 
 export const LogIn = () => {
@@ -8,7 +9,7 @@ export const LogIn = () => {
                 main: '#AA7CD2',
                 contrastText: 'white',
             },
-        },
+        }
     })
 
     return (
@@ -37,28 +38,54 @@ export const LogIn = () => {
                     border: 2,
                     overflow: 'hidden',
                     width: '40%',
-                    height: '450px',
+                    height: '500px',
                     margin: '0',
                     top: '50%',
                     left: '50%',
                     transform: 'translate(-50%, -50%)',
                 }}>
                     <Box sx = {{
-                        p: 2,
+                        p: 1,
                         backgroundColor: '#EAEBEB',
                         color: 'black',
                         width: '50%',
                     }}>
                         <Typography variant="h5">
-                            <p className="kanit">Login</p>   
+                            <p className="kanit" style = {{
+                                marginLeft: '1.5vw',
+                            }}>Log In</p>   
                         </Typography>
                         <Typography variant="h6">
-                            <p className = "exo">Username</p>   
+                            <p className = "exo" style = {{
+                                marginLeft: '1.5vw',
+                            }}>Username</p>   
                         </Typography>
-                        <TextField variant="outlined"/>
+                        <input type="text" style={{
+                            borderRadius: '30px',
+                            border: '0',
+                            width: '80%',
+                            height: '6%',
+                            padding: '8px',
+                            display: 'flex',
+                            margin: 'auto',
+                        }}/>
                         <Typography variant="h6">
-                            <p className = "exo">Password</p>   
+                            <p className = "exo" style = {{
+                                marginLeft: '1.5vw',
+                            }}>Password</p>   
                         </Typography>
+                        <input type="text" style={{
+                            borderRadius: '30px',
+                            border: '0',
+                            width: '80%',
+                            height: '6%',
+                            padding: '8px',
+                            display: 'flex',
+                            margin: 'auto',
+                        }}/>
+                        <div style = {{
+                            margin: '40px'
+                        }}></div>
                         <ThemeProvider theme = {purple}>
                             <Button variant = "contained" disableElevation sx = {{
                                 width: '80%',
@@ -66,8 +93,34 @@ export const LogIn = () => {
                                 margin: 'auto',
                                 display: 'flex',
                                 color: 'black',
-                            }}>Sign In</Button>
+                                borderRadius: '20px',
+                            }}>
+                                <p className="exo" style = {{
+                                    fontSize: '20px',
+                                }}>Sign In</p>
+                            </Button>
                         </ThemeProvider>
+
+                        <Box sx = {{
+                            marginTop: '20px',
+                            display: 'flex',
+                        }}>
+                            <Checkbox sx = {{
+                                marginLeft: '1.5vw',
+                            }}/>
+                            <p className="yantraLight" style = {{
+                                fontSize: '10px',
+                                marginTop: 'auto',
+                                marginBottom: 'auto',
+                            }}>Remember Me</p>
+                            <p className="yantraLight" style = {{
+                                fontSize: '10px',
+                                marginTop: 'auto',
+                                marginBottom: 'auto',
+                                marginLeft: 'auto',
+                                marginRight: '2vw',
+                            }}>Forgot Password</p>
+                        </Box>
                     </Box>
                     <Box sx = {{
                         p: 2,
@@ -76,23 +129,32 @@ export const LogIn = () => {
                         borderLeft: 2,
                         width: '50%',
                         textAlign: 'center',
-
                     }}>
                         <Typography variant="h3" sx = {{
                             color: 'white'
                         }}>
-                            <p>HELLO</p>   
+                            <p className="yantraBold" style = {{
+                                fontSize: '60px',
+                                letterSpacing: '2px',
+                            }}>HELLO</p>   
                         </Typography>
                         <Typography variant="subtitle1" sx = {{
                             color: 'white'
                         }}>
-                            <p>New User?</p>   
+                            <p className = "yantraLight">New User?</p>   
                         </Typography>
-                        <Typography variant="h6" sx = {{
-                            color: 'white'
+                        <Button variant = "outlined" sx = {{
+                            color: 'white',
+                            borderColor: 'black',
+                            borderRadius: '15px',
+                            marginTop: '20px',
                         }}>
-                            <p>Sign Up</p>   
-                        </Typography>
+                            <p className = "yantraLight" style = {{
+                            margin: '2px'
+                            }}>
+                                Sign Up
+                            </p>
+                        </Button>
                     </Box>
                 </Box>
             </Box>
